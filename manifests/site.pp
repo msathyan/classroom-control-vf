df9cfebc6e03fcca29cfd4bc55a303aaf8dff0d7
msathyan <sathym@gmail.com>
2016-07-19 15:33:37 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -42,5 +42,9 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
-  notify { "Hello, my name is ${::hostname}": }
+  # notify { "Hello, my name is ${::hostname}": }
+  file { '/etc/motd':
+    owner => 'demo',
+    content => "Learing Pupprt\n",
+    }
 }
