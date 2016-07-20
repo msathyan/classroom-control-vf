e5bcdb4757acd98bc4442c1ffb4ceb4e4e8fbfbe
msathyan <sathym@gmail.com>
2016-07-20 10:35:10 -0700

    Update init.pp

--- site/users/manifests/init.pp
+++ site/users/manifests/init.pp
@@ -1,10 +1,5 @@
 class users {
-user { 'fundamentals':
-ensure => present,
-home => '/home/fundamentals',
-password => '!!',
-password_max_age => '99999',
-password_min_age => '0',
-shell => '/bin/bash',
-}
+  user { 'fundamentals':
+    ensure => present,
+  }
 }
