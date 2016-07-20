e388fc0c761278ed3cc7fec50606607a9c1af4a3
msathyan <sathym@gmail.com>
2016-07-20 11:36:22 -0700

    Create init.pp

---
+++ site/skeleton/manifests/init.pp
@@ -0,0 +1,14 @@
+class skeleton {
+file {'/etc/skel':
+  ensure  => directory,
+  }
+
+file { '/etc/skel/.bashrc':
+  ensure  => file,
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+  require => File['/etc/skel']
+  source  => 'puppet:///modules/skeleton/bashrc',
+}
+}
