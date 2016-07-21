5b7f7513848fec0ddc5f89353bed1e4b01dca570
msathyan <sathym@gmail.com>
2016-07-21 14:41:45 -0700

    Create managed_user.pp

---
+++ site/users/manifests/managed_user.pp
@@ -0,0 +1,10 @@
+define user::managed_user (
+    $username = $title,
+    $homedir = "/home/$title",
+    $usergroup = 'foo',
+) {
+  file { "/home/${title}/.bashrc":
+    ensure  => file,
+    content => template('user/bashrc.erb'),
+  }
+}
