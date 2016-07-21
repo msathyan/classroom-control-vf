4510eb8015a4063bfcfba9498bf4975fa650d98f
msathyan <sathym@gmail.com>
2016-07-21 15:02:21 -0700

    Update init.pp

--- site/users/manifests/init.pp
+++ site/users/manifests/init.pp
@@ -1,5 +1,9 @@
-class users {
-  user { 'fundamentals':
-    ensure => present,
-  }
-}
+#class users {
+#  user { 'fundamentals':
+#    ensure => present,
+#  }
+#}
+
+users::managed_user { "jose"; }
+users::managed_user { "alice": }
+users::managed_user { "chen": }
