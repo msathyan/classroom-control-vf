cf43c6869628492e5c2698b57d273528c206254b
msathyan <sathym@gmail.com>
2016-07-21 10:12:35 -0700

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,42 +1,33 @@
 class nginx {
- file { '/etc/nginx/nginx.conf':
-  ensure  => file,
+File{
   owner   => 'root',
   group   => 'root',
   mode    => '0644',
+}
+
+ file { '/etc/nginx/nginx.conf':
+  ensure  => file,
   source  => 'puppet:///modules/nginx/nginx.conf',
   require => Package['nginx'],
  }
 
  file { '/etc/nginx/conf.d':
   ensure  => directory,
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
   require => File['/etc/nginx/nginx.conf'],
  }
 
  file { '/etc/nginx/conf.d/default.conf':
   ensure  => file,
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
   source  => 'puppet:///modules/nginx/default.conf',
  }
  
  file { '/var/www':
   ensure  => directory,
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
   require => File['/etc/nginx/conf.d/default.conf'],
  }
  
  file { '/var/www/index.html':
   ensure  => file,
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
   source  => 'puppet:///modules/nginx/index.html',
  }
  
