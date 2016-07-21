f628ac98c18e7c8184031dfd83bbeda48aa531af
msathyan <sathym@gmail.com>
2016-07-21 15:26:01 -0700

    Update managed_user.pp

--- site/users/manifests/managed_user.pp
+++ site/users/manifests/managed_user.pp
@@ -8,11 +8,24 @@ define users::managed_user (
     groups  => $usergroup,
     home    => $homedir,
   }
+  file { "/home/$title" :
+    ensure => directory,
+    owner  => $title,
+    group  => $title,
+    mode   => '0700',
+}
+
   file { "/home/${title}/.bashrc":
     ensure => present,
+    owner  => $title,
+    group  => $title,
+    mode   => '0640',
     content => template('users/bashrc.erb'),
   }
   file { "/home/${title}/.ssh":
     ensure => directory,
+    owner  => $title,
+    group  => $title,
+    mode   => '0600',
   }
 }
