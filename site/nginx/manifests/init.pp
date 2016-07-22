6a417d52bd42f016a8ace770fca888f4e6ad9b98
msathyan <sathym@gmail.com>
2016-07-21 18:02:30 -0700

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,43 +1,45 @@
 class nginx {
-File{
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
 }
+#class nginx {
+#File{
+#  owner   => 'root',
+#  group   => 'root',
+#  mode    => '0644',
+#}
 
- file { '/etc/nginx/nginx.conf':
-  ensure  => file,
-  source  => 'puppet:///modules/nginx/nginx.conf',
-  require => Package['nginx'],
- }
+ #file { '/etc/nginx/nginx.conf':
+#  ensure  => file,
+#  source  => 'puppet:///modules/nginx/nginx.conf',
+#  require => Package['nginx'],
+# }
 
- file { '/etc/nginx/conf.d':
-  ensure  => directory,
-  require => File['/etc/nginx/nginx.conf'],
- }
+# file { '/etc/nginx/conf.d':
+#  ensure  => directory,
+#  require => File['/etc/nginx/nginx.conf'],
+# }
 
- file { '/etc/nginx/conf.d/default.conf':
-  ensure  => file,
-  source  => 'puppet:///modules/nginx/default.conf',
- }
+# file { '/etc/nginx/conf.d/default.conf':
+#  ensure  => file,
+#  source  => 'puppet:///modules/nginx/default.conf',
+# }
  
- file { '/var/www':
-  ensure  => directory,
-  require => File['/etc/nginx/conf.d/default.conf'],
- }
+# file { '/var/www':
+#  ensure  => directory,
+#  require => File['/etc/nginx/conf.d/default.conf'],
+# }
  
- file { '/var/www/index.html':
-  ensure  => file,
-  source  => 'puppet:///modules/nginx/index.html',
- }
+# file { '/var/www/index.html':
+#  ensure  => file,
+#  source  => 'puppet:///modules/nginx/index.html',
+# }
  
- package { 'nginx':
-  ensure => present,
- }
+ #package { 'nginx':
+#  ensure => present,
+# }
 
- service { 'nginx':
-  ensure    => running,
-  enable    => true,
-  subscribe => File['/etc/nginx/conf.d/default.conf','/etc/nginx/nginx.conf'],
- }
-}
+# service { 'nginx':
+#  ensure    => running,
+#  enable    => true,
+#  subscribe => File['/etc/nginx/conf.d/default.conf','/etc/nginx/nginx.conf'],
+# }
+#}
